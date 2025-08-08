bb2379dfebc12aea126c6d05253ae08eba5393d7
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2025-08-08 18:56:49 +0530

    add gradient border to Footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,10 @@ import { FaHeart as HeartIcon } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="border-t border-neutral-600 mt-18 text-center text-sm text-[#b7b7b7] pt-6">
+    <footer className="mt-18 text-center text-sm text-[#b7b7b7] pt-6 relative">
+      {/* Gradient border */}
+      <div className="absolute top-0 left-0 w-full h-[1px] bg-gradient-to-r from-transparent via-neutral-600 to-transparent" />
+
       <p className="flex justify-center items-center gap-1 flex-wrap">
         © {new Date().getFullYear()} — Made with{" "}
         <HeartIcon className="hover:text-white cursor-pointer" size={12} /> by{" "}
@@ -15,6 +18,7 @@ export default function Footer() {
           @kuldp18
         </a>
       </p>
+
       <div className="mt-2 flex justify-center gap-4 flex-wrap">
         <a
           href="https://github.com/kuldp18"
